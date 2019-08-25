5449cfba631a4933feaf887958580ae54f88d1d0
Dr. Alok Pratap <quicktwit@gmail.com>
2019-08-25 17:56:19 +0900

    Rear motor test with IR sensor with ESP-WROOM-32

---
+++ firmware/src/esp32motortest.ino
@@ -0,0 +1,99 @@
+// Motor A
+int motor1Pin1 = 4; 
+int motor1Pin2 = 0; 
+int enable1Pin = 5; 
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
+
+//IR sensor
+const int dataIN = 14; //IR sensor INPUT
+
+
+unsigned long prevmillis; // To store time
+unsigned long duration; // To store time difference
+unsigned long refresh; // To store time for refresh of reading
+
+int rpm; // RPM value
+
+boolean currentstate; // Current state of IR input scan
+boolean prevstate; // State of IR sensor in previous scan
+
+void setup() {
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+  
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(enable1Pin, pwmChannel);
+
+  Serial.begin(115200);
+  pinMode(dataIN,INPUT);       
+  prevmillis = 0;
+  prevstate = LOW; 
+
+  // testing
+  Serial.print("Testing DC Motor...");
+}
+
+void loop() {
+   // RPM Measurement
+ currentstate = digitalRead(dataIN); // Read IR sensor state
+ if( prevstate != currentstate) // If there is change in input
+   {
+     if( currentstate == HIGH ) // If input only changes from LOW to HIGH
+       {
+         duration = ( micros() - prevmillis ); // Time difference between revolution in microsecond
+         rpm = (60000000/duration); // rpm = (1/ time millis)*1000*1000*60;
+         prevmillis = micros(); // store time for nect revolution calculation
+       }
+   }
+  prevstate = currentstate; // store this scan (prev scan) data for next scan
+  if( ( millis()-refresh ) >= 100 )
+  {
+     Serial.print("RPM: ");
+     Serial.println(rpm);  
+  }
+  // Move the DC motor forward at maximum speed
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
+  delay(2000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+
+  // Move DC motor backwards at maximum speed
+  Serial.println("Moving Backwards");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW); 
+  delay(2000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+
+  // Move DC motor forward with increasing speed
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  while (dutyCycle <= 255){
+    ledcWrite(pwmChannel, dutyCycle);   
+    Serial.print("Forward with duty cycle: ");
+    Serial.println(dutyCycle);
+    dutyCycle = dutyCycle + 5;
+    delay(500);
+  }
+  dutyCycle = 200;
+}
