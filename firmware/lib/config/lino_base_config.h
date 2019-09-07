4cef5e4b842e8e7ff7ad8d4d322d06801537f028
Dr. Alok Pratap <quicktwit@gmail.com>
2019-09-08 01:45:44 +0900

    added encoder pins for 2 rear motors

--- firmware/lib/config/lino_base_config.h
+++ firmware/lib/config/lino_base_config.h
@@ -47,14 +47,6 @@ ROBOT ORIENTATION
   #define MOTOR2_IN_A 8
   #define MOTOR2_IN_B 6
 
-  #define MOTOR3_PWM 22
-  #define MOTOR3_IN_A 23
-  #define MOTOR3_IN_B 0
-
-  #define MOTOR4_PWM 4
-  #define MOTOR4_IN_A 2
-  #define MOTOR4_IN_B 3
-
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
 #endif 
